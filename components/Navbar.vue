f913858a074337e8e2abe9927fff5bb612ad1d65
cristianA123 <cristianchipan2@gmail.com>
2024-01-26 17:07:39 -0500

    init proyect

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <VAppBar>
+        <template v-slot:title>
+
+            <h1 class="text-xl font-bold underline">
+                Hello world!
+            </h1>
+        </template>
+    </VAppBar>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
+    '~/assets/css/main.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  modules:[
+    '@pinia/nuxt',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
